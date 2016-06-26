f33f4c8d04d90c13f5d69cfcea9d35cd552ffe18
Hervé BREDIN <bredin@limsi.fr>
2016-06-26 14:40:46 +0200

    setup: update to pyannote.core 0.6.6

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     namespace_packages=['pyannote'],
     packages=find_packages(),
     install_requires=[
-        'pyannote.core >= 0.6.5',
+        'pyannote.core >= 0.6.6',
     ],
     # versioneer
     version=versioneer.get_version(),
