9b59990a107a453140c2cea684d23cf95496a79c
Hervé BREDIN <bredin@limsi.fr>
2016-07-05 14:36:45 +0200

    refactor: generate inter-show triplets

--- pyannote/audio/embedding/generator.py
+++ pyannote/audio/embedding/generator.py
@@ -30,34 +30,152 @@ import itertools
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
 from pyannote.generators.batch import BaseBatchGenerator
-from pyannote.generators.fragment import RandomSegmentsPerLabel
-from ..features.yaafe import YaafeBatchGenerator
+from pyannote.generators.fragment import SlidingLabeledSegments
+from ..features.yaafe import YaafeFileBasedBatchGenerator
 
 
-class _YaafeTripletGenerator(object):
+class YaafeTripletLossGenerator(object):
+    """Yaafe sequence generator for triplet loss
 
-    def __init__(self, yaafe_feature_extractor, embedding, margin=0.2, duration=3.2, per_label=40, normalize=False):
-        super(_YaafeTripletGenerator, self).__init__()
+    Parameter
+    ---------
+    extractor: YaafeFeatureExtractor
+        Yaafe feature extraction (e.g. YaafeMFCC())
+    file_generator: iterable
+        File generator.
+    embedding: TripletLossSequenceEmbedding
+        Triplet loss sequence embedding.
+    duration: float, optional
+        Sequence duration. Defaults to 3 seconds.
+    overlap: float, optional
+        Sequence overlap ratio. Defaults to 0 (no overlap).
+    normalize: boolean, optional
+        When True, normalize sequence (z-score). Defaults to False.
+    n_labels: int, optional
+        Number of labels per batch. Defaults to 40.
+    n_samples: int, optional
+        Number of samples per label. Defaults to 40.
+    embedding_batch_size: int, optional
+        Batch size to use for embedding. Defaults to 32.
+    """
 
-        generator = RandomSegmentsPerLabel(
-            duration=duration,
-            per_label=per_label,
-            yield_label=True)
+    def __init__(self, extractor, file_generator, embedding,
+                 duration=3.0, overlap=0.0, normalize=False,
+                 n_labels=40, n_samples=40, embedding_batch_size=32):
+        super(YaafeTripletLossGenerator, self).__init__()
 
-        self.batch_generator = YaafeBatchGenerator(
-            yaafe_feature_extractor,
-            generator,
-            batch_size=-1,
-            normalize=normalize)
-
-        self.margin = margin
+        self.extractor = extractor
+        self.file_generator = file_generator
         self.embedding = embedding
+        self.duration = duration
+        self.overlap = overlap
+        self.normalize = normalize
+        self.n_labels = n_labels
+        self.n_samples = n_samples
+        self.embedding_batch_size = embedding_batch_size
+
+        fragment_generator = SlidingLabeledSegments(
+            duration=self.duration,
+            step=(1 - self.overlap) * self.duration)
+
+        self.batch_sequence_generator_ = YaafeFileBasedBatchGenerator(
+            self.extractor,
+            fragment_generator,
+            batch_size=-1,
+            normalize=self.normalize)
+
+        self.triplet_generator_ = self.iter_triplets()
+
+    def iter_triplets(self):
+
+        # generate all sequences first
+        X, y = [], []
+        for batch_sequences, batch_labels in self.batch_sequence_generator_(self.file_generator):
+            X.append(batch_sequences)
+            y.append(batch_labels)
+        X = np.vstack(X)
+        y = np.hstack(y)
+
+        # unique labels
+        unique, y, counts = np.unique(y, return_inverse=True, return_counts=True)
+        n = len(unique)
+
+        # infinite loop
+        while True:
+
+            # shuffle labels
+            shuffled_labels = np.random.choice(n, size=n, replace=False)
+
+            # take them n_labels per n_labels
+            for k in range(n / self.n_labels):
+                from_label = k * self.n_labels
+                to_label = (k+1) * self.n_labels
+                labels = shuffled_labels[from_label: to_label]
+
+            # # -- select a subset of labels at random
+            # labels = np.random.choice(
+            #     n, size=self.n_labels, replace=False, p=None)
+
+                # select min(n_samples, count) sequences
+                # at random for each label
+                indices = []
+                n_samples = []
+                for label in labels:
+                    n_samples.append(min(self.n_samples, counts[label]))
+                    i = np.random.choice(
+                        np.where(y == label)[0],
+                        size=n_samples[-1],
+                        replace=True)
+                    indices.append(i)
+                indices = np.hstack(indices)
+                n_samples = np.hstack([[0], np.cumsum(n_samples)])
+
+                # pre-compute distances
+                sequences = X[indices]
+                embeddings = self.embedding.transform(
+                    sequences, batch_size=self.embedding_batch_size)
+                distances = squareform(pdist(embeddings, metric='euclidean'))
+
+                for i in range(self.n_labels):
+
+                    positives = list(range(n_samples[i], n_samples[i+1]))
+                    negatives = list(range(n_samples[i])) + list(range(n_samples[i+1], n_samples[-1]))
+
+                    # positives = list(range(i * self.n_samples,
+                    #                        (i+1) * self.n_samples))
+                    # negatives = list(range(i * self.n_samples)) + \
+                    #             list(range((i+1) * self.n_samples,
+                    #                        self.n_labels * self.n_samples))
+
+                    # loop over all (anchor, positive) pairs for current label
+                    for anchor, positive in itertools.combinations(positives, 2):
+
+                        # find all negatives within the margin
+                        d = distances[anchor, positive]
+                        within_margin = np.where(
+                            distances[anchor, negatives] < d + self.embedding.margin)[0]
+
+                        # choose one at random (if at least one exists)
+                        if len(within_margin) < 1:
+                            continue
+                        negative = negatives[np.random.choice(within_margin)]
+
+                        yield [sequences[anchor], sequences[positive], sequences[negative]], 1
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.__next__()
+
+    def __next__(self):
+        return next(self.triplet_generator_)
 
     def get_shape(self):
-        return self.batch_generator.get_shape()
+        return self.batch_sequence_generator_.get_shape()
 
     def signature(self):
-        shape = self.batch_generator.get_shape()
+        shape = self.batch_sequence_generator_.get_shape()
         return (
             [
                 {'type': 'sequence', 'shape': shape},
@@ -67,49 +185,42 @@ class _YaafeTripletGenerator(object):
             {'type': 'boolean'}
          )
 
-    def from_protocol_item(self, protocol_item):
 
-        for batch_sequences, batch_labels in self.batch_generator.from_protocol_item(protocol_item):
+class YaafeTripletLossBatchGenerator(BaseBatchGenerator):
+    """Yaafe batch generator for triplet loss
 
-            batch_embeddings = self.embedding.transform(batch_sequences)
-            batch_distances = squareform(pdist(batch_embeddings, metric='euclidean'))
-
-            labels, unique_inverse = np.unique(batch_labels, return_inverse=True)
-
-            for i, label in enumerate(labels):
-                positives = np.where(unique_inverse == i)[0]
-                negatives = np.where(unique_inverse != i)[0]
-
-                # loop over all (anchor, positive) pairs for current label
-                for anchor, positive in itertools.combinations(positives, 2):
-
-                    # find all negatives within the margin
-                    d = batch_distances[anchor, positive]
-                    within_margin = np.where(
-                        batch_distances[anchor, negatives] < d + self.margin)[0]
-
-                    # choose one at random (if at least one exists)
-                    if len(within_margin) < 1:
-                        continue
-                    negative = negatives[np.random.choice(within_margin)]
-                    yield [batch_sequences[anchor], batch_sequences[positive], batch_sequences[negative]], 1
-
-
-class YaafeTripletBatchGenerator(BaseBatchGenerator):
+    Parameter
+    ---------
+    extractor: YaafeFeatureExtractor
+        Yaafe feature extraction (e.g. YaafeMFCC())
+    embedding: TripletLossSequenceEmbedding
+        Triplet loss sequence embedding.
+    duration: float, optional
+        Sequence duration. Defaults to 3 seconds.
+    overlap: float, optional
+        Sequence overlap ratio. Defaults to 0 (no overlap).
+    normalize: boolean, optional
+        When True, normalize sequence (z-score). Defaults to False.
+    n_labels: int, optional
+        Number of labels per batch. Defaults to 40.
+    n_samples: int, optional
+        Number of samples per label. Defaults to 40.
+    batch_size: int, optional
+        Number of sequences per batch. Defaults to 32
     """
-    Parameters
-    ----------
-    extractor : YaafeFeatureExtractor
-    embedding : TripletLossSequenceEmbedding
-    """
-    def __init__(self, extractor, embedding, duration=3.2, per_label=40, batch_size=32, normalize=False):
-        generator = _YaafeTripletGenerator(
-            extractor,
-            embedding,
-            duration=duration,
-            per_label=per_label,
-            normalize=normalize)
-        super(YaafeTripletBatchGenerator, self).__init__(generator, batch_size=batch_size)
+
+    def __init__(self, file_generator, extractor, embedding,
+                 duration=3.0, overlap=0.0, normalize=False,
+                 n_labels=40, n_samples=40,
+                 batch_size=32):
+
+        self.triplet_generator_ = YaafeTripletLossGenerator(
+            extractor, file_generator, embedding,
+            duration=duration, overlap=overlap, normalize=normalize,
+            n_labels=n_labels, n_samples=n_samples)
+
+        super(YaafeTripletLossBatchGenerator, self).__init__(
+            self.triplet_generator_, batch_size=batch_size)
 
     def get_shape(self):
-        return self.generator.get_shape()
+        return self.triplet_generator_.get_shape()
