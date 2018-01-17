fix: fix a bug in pytorch-based pdist computation

pyannote/audio/embedding/approaches_pytorch/triplet_loss.py
@@ -118,21 +118,20 @@ class TripletLoss(object):
         n_sequences, _ = fX.size()
         distances = []
 
-        for i in range(1, n_sequences):
+        for i in range(n_sequences - 1):
 
             if self.metric in ('cosine', 'angular'):
-
                 d = 1. - F.cosine_similarity(
-                    fX[i, :].expand(i, -1), fX[:i, :],
-                    dim=1, eps=1e-8)
+                    fX[i, :].expand(n_sequences - 1 - i, -1),
+                    fX[i+1:, :], dim=1, eps=1e-8)
 
                 if self.metric == 'angular':
                     d = torch.acos(torch.clamp(1. - d, -1 + 1e-6, 1 - 1e-6))
 
             elif self.metric == 'euclidean':
                 d = F.pairwise_distance(
-                    fX[i, :].expand(i, -1), fX[:i, :],
-                    p=2, eps=1e-06).view(-1)
+                    fX[i, :].expand(n_sequences - 1 - i, -1),
+                    fX[i+1:, :], p=2, eps=1e-06).view(-1)
 
             distances.append(d)
 
@@ -165,10 +164,10 @@ class TripletLoss(object):
         for anchor, y_anchor in enumerate(y):
 
             d = distances[anchor]
-            pos = np.where(y == y_anchor)[0]
-            pos = [p for p in pos if p != anchor]
 
             # hardest positive
+            pos = np.where(y == y_anchor)[0]
+            # pos = [p for p in pos if p != anchor]
             positive = int(pos[np.argmax(d[pos])])
 
             # hardest negative
@@ -233,14 +232,18 @@ class TripletLoss(object):
             Average triplet loss.
         """
 
+        # estimate total number of embeddings from pdist shape
+        n = int(.5 * (1 + np.sqrt(1 + 8 * len(distances))))
+        n = [n] * len(anchors)
+
         # convert indices from squared matrix
         # to condensed matrix referential
-        p = list(map(to_condensed, anchors, positives))
-        n = list(map(to_condensed, anchors, negatives))
+        pos = list(map(to_condensed, n, anchors, positives))
+        neg = list(map(to_condensed, n, anchors, negatives))
 
         # compute raw triplet loss (no margin, no clamping)
         # the lower, the better
-        delta = distances[p] - distances[n]
+        delta = distances[pos] - distances[neg]
 
         # clamp triplet loss
         if self.clamp == 'positive':

pyannote/audio/embedding/utils.py
@@ -66,18 +66,24 @@ def cdist(fX_trn, fX_tst, metric='euclidean', **kwargs):
 
     return scipy.spatial.distance.cdist(
         fX_trn, fX_tst, metric=metric, **kwargs)
-def to_condensed(i, j):
+
+
+def to_condensed(n, i, j):
     """Compute index in condensed pdist matrix
 
-           0 1 2 3 4
-        0  . . . . .
-        1  0 . . . .  --> 0 1 2 3 4 5 6 7 8 9
-        2  1 2 . . .                      ^
-      > 3  3 4 5 . .
-        4  6 7 8 9 .
-               ^
+                V
+        0 | . 0 1 2 3
+     -> 1 | . . 4 5 6 <-   ==>   0 1 2 3 4 5 6 7 8 9
+        2 | . . . 7 8                    ^
+        3 | . . . . 9
+        4 | . . . . .
+           ----------
+            0 1 2 3 4
+
     Parameters
     ----------
+    n : int
+        Number of inputs in squared pdist matrix
     i, j : int
         Indices in squared pdist matrix
 
@@ -86,9 +92,7 @@ def to_condensed(i, j):
     k : int
         Index in condensed pdist matrix
     """
-
-    assert i != j, "no diagonal elements in condensed matrix"
-    if i < j:
-        i, j = j, i
-
-    return j + int(.5 * (i * (i - 1)))
+    if i == j:
+        raise ValueError('i and j should be different.')
+    i, j = min(i, j), max(i, j)
+    return int(i * n -i * i / 2 - 3 * i / 2 + j - 1)