55c81f77542b42fc168502c94afcb4f0864e37da
Hervé Bredin <bredin@limsi.fr>
2018-02-14 10:02:19 +0100

    feat: add (fast) equal/minimum/maximum/average metrics to cdist

--- pyannote/audio/embedding/utils.py
+++ pyannote/audio/embedding/utils.py
@@ -93,12 +93,44 @@ def pdist(fX, metric='euclidean', **kwargs):
         return scipy.spatial.distance.pdist(fX, metric=metric, **kwargs)
 
 
+def _cdist_func_1D(X_trn, X_tst, func):
+    """Helper function for cdist"""
+
+    X_trn = X_trn.squeeze()
+    n_items, = X_tst.shape
+    X_tst = X_tst.squeeze()
+
+    return np.vstack(func(x_trn, X_tst) for x_trn in iter(X_trn))
+
+
 def cdist(fX_trn, fX_tst, metric='euclidean', **kwargs):
+    """Same as scipy.spatial.distance.cdist with support for additional metrics
+
+    * 'angular': pairwise angular distance
+    * 'equal':   pairwise equality check (only for 1-dimensional fX)
+    * 'minimum': pairwise minimum (only for 1-dimensional fX)
+    * 'maximum': pairwise maximum (only for 1-dimensional fX)
+    * 'average': pairwise average (only for 1-dimensional fX)
+    """
 
     if metric == 'angular':
         cosine = scipy.spatial.distance.cdist(
             fX_trn, fX_tst, metric='cosine', **kwargs)
         return np.arccos(np.clip(1.0 - cosine, -1.0, 1.0))
+
+    elif metric == 'equal':
+        return _cdist_func_1D(fX_trn, fX_tst,
+                              lambda x_trn, X_tst: x_trn == X_tst)
+
+    elif metric == 'minimum':
+        return _cdist_func_1D(fX_trn, fX_tst, np.minimum)
+
+    elif metric == 'maximum':
+        return _cdist_func_1D(fX_trn, fX_tst, np.maximum)
+
+    elif metric == 'average':
+        return _cdist_func_1D(fX_trn, fX_tst,
+                              lambda x_trn, X_tst: .5 * (x_trn + X_tst))
 
     return scipy.spatial.distance.cdist(
         fX_trn, fX_tst, metric=metric, **kwargs)
