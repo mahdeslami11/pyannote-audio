53902dafe3bc59fdb06034dd4555c6873c182a12
Hervé Bredin <bredin@limsi.fr>
2018-06-29 14:52:44 +0200

    setup: remove support for Python 3.5

--- setup.py
+++ setup.py
@@ -85,8 +85,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering"
     ],
 )
