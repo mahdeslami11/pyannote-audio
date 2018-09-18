8ca88136436b25f27058588c939e9f04212dfa41
Hervé Bredin <bredin@limsi.fr>
2018-09-18 16:29:54 +0200

    feat: add generic VGGVox model

--- pyannote/audio/embedding/models.py
+++ pyannote/audio/embedding/models.py
@@ -27,6 +27,7 @@
 # Hervé BREDIN - http://herve.niderb.fr
 
 
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -194,63 +195,165 @@ class TristouNet(nn.Module):
 
 
 class VGGVox(nn.Module):
+    """VGGVox implementation
 
-    def __init__(self, n_features, output_dim=128):
+    Reference
+    ---------
+    Arsha Nagrani, Joon Son Chung, Andrew Zisserman. "VoxCeleb: a large-scale
+    speaker identification dataset."
+
+    """
+
+    @staticmethod
+    def _output_shape(input_shape, kernel_size, stride=1, padding=0, dilation=1):
+        """Predict output shape"""
+
+        if not isinstance(kernel_size, tuple):
+            kernel_size = (kernel_size, kernel_size)
+        if not isinstance(stride, tuple):
+            stride = (stride, stride)
+        if not isinstance(padding, tuple):
+            padding = (padding, padding)
+        if not isinstance(dilation, tuple):
+            dilation = (dilation, dilation)
+
+        h_in, w_in = input_shape
+
+        h_out = (h_in + 2 * padding[0] - dilation[0] * (kernel_size[0]- 1) - 1)
+        h_out = h_out / stride[0] + 1
+
+        w_out = (w_in + 2 * padding[1] - dilation[1] * (kernel_size[1]- 1) - 1)
+        w_out = w_out / stride[1] + 1
+
+        return int(math.floor(h_out)), int(math.floor(w_out))
+
+    def __init__(self, n_features, output_dim=256):
+
+        if n_features < 97:
+            msg = (f'VGGVox expects features with at least 97 dimensions '
+                   f'(here, n_features = {n_features:d})')
+            raise ValueError(msg)
 
         super(VGGVox, self).__init__()
         self.n_features = n_features
         self.output_dim = output_dim
 
+        h = self.n_features  # 512 in VoxCeleb paper. 201 in practice.
+        w = 301 # typically 3s with 10ms steps
+
         self.conv1_ = nn.Conv2d(1, 96, (7, 7), stride=(2, 2), padding=1)
+        # 254 x 148 when n_features = 512
+        # 99 x 148 when n_features = 201
+        h, w = self._output_shape((h, w), (7, 7), stride=(2, 2), padding=1)
+
         self.bn1_ = nn.BatchNorm2d(96)
         self.mpool1_ = nn.MaxPool2d((3, 3), stride=(2, 2))
+        # 126 x 73 when n_features = 512
+        # 49 x 73 when n_features = 201
+        h, w = self._output_shape((h, w), (3, 3), stride=(2, 2))
+
         self.conv2_ = nn.Conv2d(96, 256, (5, 5), stride=(2, 2), padding=1)
+        # 62 x 36 when n_features = 512
+        # 24 x 36 when n_features = 201
+        h, w = self._output_shape((h, w), (5, 5), stride=(2, 2), padding=1)
+
         self.bn2_ = nn.BatchNorm2d(256)
         self.mpool2_ = nn.MaxPool2d((3, 3), stride=(2, 2))
+        # 30 x 17 when n_features = 512
+        # 11 x 17 when n_features = 201
+        h, w = self._output_shape((h, w), (3, 3), stride=(2, 2))
+
         self.conv3_ = nn.Conv2d(256, 256, (3, 3), stride=(1, 1), padding=1)
+        # 30 x 17 when n_features = 512
+        # 11 x 17 when n_features = 201
+        h, w = self._output_shape((h, w), (3, 3), stride=(1, 1), padding=1)
+
         self.bn3_ = nn.BatchNorm2d(256)
+
         self.conv4_ = nn.Conv2d(256, 256, (3, 3), stride=(1, 1), padding=1)
+        # 30 x 17 when n_features = 512
+        # 11 x 17 when n_features = 201
+        h, w = self._output_shape((h, w), (3, 3), stride=(1, 1), padding=1)
+
         self.bn4_ = nn.BatchNorm2d(256)
+
         self.conv5_ = nn.Conv2d(256, 256, (3, 3), stride=(1, 1), padding=1)
+        # 30 x 17 when n_features = 512
+        # 11 x 17 when n_features = 201
+        h, w = self._output_shape((h, w), (3, 3), stride=(1, 1), padding=1)
+
         self.bn5_ = nn.BatchNorm2d(256)
+
         self.mpool5_ = nn.MaxPool2d((5, 3), stride=(3, 2))
-        self.fc6_ = nn.Conv2d(256, 4096, (9, 1), stride=(1, 1))
-        self.bn6_ = nn.BatchNorm2d(4096)
-        self.fc7_ = nn.Conv2d(4096, 1024, (1, 1), stride=(1, 1))
-        self.bn7_ = nn.BatchNorm2d(1024)
-        self.fc8_ = nn.Conv2d(1024, self.output_dim, (1, 1), stride=(1, 1))
-        self.bn8_ = nn.BatchNorm2d(self.output_dim)
+        # 9 x 8 when n_features = 512
+        # 3 x 8 when n_features = 201
+        h, w = self._output_shape((h, w), (5, 3), stride=(3, 2))
+
+        self.fc6_ = nn.Conv2d(256, 4096, (h, 1), stride=(1, 1))
+        # 1 x 8
+        h, w = self._output_shape((h, w), (h, 1), stride=(1, 1))
+
+        self.fc7_ = nn.Linear(4096, 1024)
+        self.fc8_ = nn.Linear(1024, self.output_dim)
+
 
     def forward(self, sequences):
         """Embed sequences
 
         Parameters
         ----------
-        sequences : torch.autograd.Variable (batch_size, n_samples, n_features)
+        sequences : torch.Tensor (batch_size, n_samples, n_features)
             Batch of sequences.
 
         Returns
         -------
-        embeddings : torch.autograd.Variable (batch_size, output_dim)
+        embeddings : torch.Tensor (batch_size, output_dim)
             Batch of embeddings.
-
         """
 
+        # reshape batch to (batch_size, n_channels, n_features, n_samples)
         batch_size, n_samples, n_features = sequences.size()
+
+        if n_features != self.n_features:
+            msg = (f'Mismatch in feature dimension '
+                   f'(should be: {self.n_features:d}, is: {n_features:d})')
+            raise ValueError(msg)
+
+        if n_samples < 65:
+            msg = (f'VGGVox expects sequences with at least 65 samples '
+                   f'(here, n_samples = {n_samples:d})')
+            raise ValueError(msg)
+
         x = torch.transpose(sequences, 1, 2).view(
             batch_size, 1, n_features, n_samples)
-        x = F.relu(self.bn1_(self.conv1_(x)))
-        x = self.mpool1_(x)
-        x = F.relu(self.bn2_(self.conv2_(x)))
-        x = self.mpool2_(x)
+
+        # conv1. shape => 254 x 148 => 126 x 73
+        x = self.mpool1_(F.relu(self.bn1_(self.conv1_(x))))
+
+        # conv2. shape =>
+        x = self.mpool2_(F.relu(self.bn2_(self.conv2_(x))))
+
+        # conv3. shape = 62 x 36
         x = F.relu(self.bn3_(self.conv3_(x)))
+
+        # conv4. shape = 30 x 17
         x = F.relu(self.bn4_(self.conv4_(x)))
-        x = F.relu(self.bn5_(self.conv5_(x)))
-        x = self.mpool5_(x)
-        x = F.relu(self.bn6_(self.fc6_(x)))
-        x = torch.mean(x, dim=-1, keepdim=True)
-        x = F.relu(self.bn7_(self.fc7_(x)))
-        x = F.relu(self.bn8_(self.fc8_(x))).view(-1, self.output_dim)
+
+        # conv5. shape = 30 x 17
+        x = self.mpool5_(F.relu(self.bn5_(self.conv5_(x))))
+
+        # fc6. shape =
+        x = F.dropout(F.relu(self.fc6_(x)))
+
+        # (average) temporal pooling. shape =
+        x = torch.mean(x, dim=-1)
+
+        # fc7. shape =
+        x = x.view(x.size(0), -1)
+        x = F.dropout(F.relu(self.fc7_(x)))
+
+        # fc8. shape =
+        x = self.fc8_(x)
 
         return x
 
