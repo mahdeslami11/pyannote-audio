e3d9a3dd406ae42a62b12a46ed2cb4a1ed73cb5c
Hervé Bredin <bredin@limsi.fr>
2018-11-30 16:05:49 +0100

    fix: add missing space in error message

--- pyannote/audio/features/utils.py
+++ pyannote/audio/features/utils.py
@@ -257,7 +257,7 @@ class RawAudio(object):
         """
 
         if self.sample_rate is None:
-            msg = ('`RawAudio` needs to be instantiated with an actual'
+            msg = ('`RawAudio` needs to be instantiated with an actual '
                    '`sample_rate` if one wants to use the `crop` method.')
             raise ValueError(msg)
 
