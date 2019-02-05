9722016a0117682fa7d0d5599a8dc2f1a75f7c6a
Hervé Bredin <bredin@limsi.fr>
2019-02-05 15:23:59 +0100

    chore: remove softmax / centroidloss

--- pyannote/audio/embedding/approaches/__init__.py
+++ pyannote/audio/embedding/approaches/__init__.py
@@ -27,6 +27,5 @@
 # Hervé BREDIN - http://herve.niderb.fr
 
 from .triplet_loss import TripletLoss
-from .centroid_loss import CentroidLoss
-from .softmax import Softmax
-# from .agg_triplet_loss import AggTripletLoss
+# from .centroid_loss import CentroidLoss
+# from .softmax import Softmax
