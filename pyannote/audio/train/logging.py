31a4db8f9886e2104cd0ae7b84f1931ad1c5700b
Hervé Bredin <bredin@limsi.fr>
2019-04-11 14:57:06 +0200

    feat: log time profiling and smoothed loss

--- pyannote/audio/train/logging.py
+++ pyannote/audio/train/logging.py
@@ -35,11 +35,10 @@ from .callback import Callback
 class Logging(Callback):
     """Log loss and processing time to tensorboard and progress bar"""
 
-    def __init__(self, epochs):
+    def __init__(self, epochs, beta=0.98):
         super().__init__()
         self.epochs = epochs
-        self.losses_ = list()
-        self.times_ = list()
+        self.beta = beta
 
     def on_train_start(self, trainer):
         self.epochs_pbar_ = tqdm(
@@ -48,34 +47,68 @@ class Logging(Callback):
             unit='epoch', initial=trainer.epoch_, position=0)
 
     def on_epoch_start(self, trainer):
-        self.epochs_pbar_.update(1)
 
+        # time spent in batch generation
+        self.t_batch_ = list()
+        # time spent in forward/backward
+        self.t_model_ = list()
+
+        # loss moving average
+        self.n_batches_ = 0
+        self.loss_moving_avg_ = 0
+
+        self.epochs_pbar_.update(1)
         self.batches_pbar_ = tqdm(
             desc=f'Epoch #{trainer.epoch_}',
             total=trainer.batches_per_epoch_,
             leave=False, ncols=80,
             unit='batch', position=1)
 
+        self.t_batch_end_ = time.time()
+
     def on_batch_start(self, trainer, batch):
-        self.time_ = time.time()
+
+        # mark time just before forward/backward
+        self.t_batch_start_ = time.time()
+
+        # time spent in batch generation
+        self.t_batch_.append(
+            self.t_batch_start_ - self.t_batch_end_
+        )
 
     def on_batch_end(self, trainer, loss):
-        # TODO. use smoothed running mean instead...
-        self.losses_.append(loss)
-        self.times_.append(time.time() - self.time_)
 
-        self.batches_pbar_.set_postfix(
-            ordered_dict={'loss': np.mean(self.losses_)})
+        # mark time just after forward/backward
+        self.t_batch_end_ = time.time()
+
+        # time spent in forward/backward
+        self.t_model_.append(
+            self.t_batch_end_ - self.t_batch_start_
+        )
+
+        self.n_batches_ += 1
+        self.loss_moving_avg_ = \
+            self.beta * self.loss_moving_avg_ + (1 - self.beta) * loss
+
+        self.loss = self.loss_moving_avg_ / (1 - self.beta ** self.n_batches_)
+
+        self.batches_pbar_.set_postfix(ordered_dict={'loss': self.loss})
         self.batches_pbar_.update(1)
 
     def on_epoch_end(self, trainer):
-        # TODO. use smoothed running mean instead...
-        trainer.tensorboard_.add_scalar(
-            'loss', np.mean(self.losses_),
-            global_step=trainer.epoch_)
-        self.losses_.clear()
 
         trainer.tensorboard_.add_scalar(
-            'time/forward_backward', np.mean(self.times_),
+            'loss', self.loss,
             global_step=trainer.epoch_)
-        self.times_.clear()
+
+        trainer.tensorboard_.add_histogram(
+            'profiling/model', np.array(self.t_model_),
+            global_step=trainer.epoch_,
+            bins='fd',
+        )
+
+        trainer.tensorboard_.add_histogram(
+            'profiling/batch', np.array(self.t_batch_),
+            global_step=trainer.epoch_,
+            bins='fd',
+        )
