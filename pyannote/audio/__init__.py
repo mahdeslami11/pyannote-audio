ea1afb9f333253e412e39c0f711a6f210e7b3e01
Hervé Bredin <bredin@limsi.fr>
2019-05-27 15:24:14 +0200

    doc: add top-level docstring

--- pyannote/audio/__init__.py
+++ pyannote/audio/__init__.py
@@ -26,6 +26,59 @@
 # AUTHORS
 # Hervé BREDIN - http://herve.niderb.fr
 
+"""
+`pyannote.audio` provides
+
+  * speech activity detection
+  * speaker change detection
+  * speaker embedding
+  * speaker diarization pipeline
+
+## Installation
+
+```bash
+$ pip install pyannote.audio
+```
+
+## Citation
+
+If you use `pyannote.audio` please use the following citations.
+
+  - Speech  activity and speaker change detection
+
+        @inproceedings{Yin2017,
+          Author = {Ruiqing Yin and Herv\'e Bredin and Claude Barras},
+          Title = {{Speaker Change Detection in Broadcast TV using Bidirectional Long Short-Term Memory Networks}},
+          Booktitle = {{18th Annual Conference of the International Speech Communication Association, Interspeech 2017}},
+          Year = {2017},
+          Month = {August},
+          Address = {Stockholm, Sweden},
+          Url = {https://github.com/yinruiqing/change_detection}
+        }
+
+  - Speaker embedding
+
+        @inproceedings{Bredin2017,
+            author = {Herv\'{e} Bredin},
+            title = {{TristouNet: Triplet Loss for Speaker Turn Embedding}},
+            booktitle = {42nd IEEE International Conference on Acoustics, Speech and Signal Processing, ICASSP 2017},
+            year = {2017},
+            url = {http://arxiv.org/abs/1609.04301},
+        }
+
+  - Speaker diarization pipeline
+
+        @inproceedings{Yin2018,
+          Author = {Ruiqing Yin and Herv\'e Bredin and Claude Barras},
+          Title = {{Neural Speech Turn Segmentation and Affinity Propagation for Speaker Diarization}},
+          Booktitle = {{19th Annual Conference of the International Speech Communication Association, Interspeech 2018}},
+          Year = {2018},
+          Month = {September},
+          Address = {Hyderabad, India},
+        }
+
+"""
+
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
