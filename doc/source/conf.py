92ddda516808a4a99f26720353a1a8a1f18d8915
Hervé Bredin <bredin@limsi.fr>
2019-12-14 13:11:44 +0100

    doc: switch to RTD theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
 ]
 
 napoleon_google_docstring = True
@@ -102,7 +103,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
