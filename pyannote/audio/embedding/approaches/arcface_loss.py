mahdeslami11/pyannote-audio

commit 55bcf77105abbaa9f775cc7f6e03b993e60d1c09

BREAKING: refactor pyannote.audio.embedding.approaches
* chore: rename EmbeddingApproach to RepresentationLearning
* feat: inherit from EmbeddingApproach.get_batch_generator
* feat: add per_turn option to all approaches (for aggregation learning)
* BREAKING: remove support for variable length sequences

pyannote/audio/embedding/approaches/arcface_loss.py
@@ -32,7 +32,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from .classification import Classification
 
-#torch.autograd.set_detect_anomaly(True)
 
 class ArcLinear(nn.Module):
     """Additive Angular Margin linear module (ArcFace)
@@ -93,23 +92,27 @@ class ArcLinear(nn.Module):
         one_hot.scatter_(1, y, 1.0)
         # project margin differences into cosθj
         return self.s * (cos_theta_j + one_hot * (cos_theta_yi_margin - cos_theta_yi))
-        #cos_theta_j += one_hot * (cos_theta_yi_margin - cos_theta_yi)
-        # apply the scaling
-        #cos_theta_j = self.s * cos_theta_j
-        #return cos_theta_j
 
 
 class AdditiveAngularMarginLoss(Classification):
-    """Train embeddings as last hidden layer of an additive angular margin loss classifier
+    """Additive angular margin loss
+
+    TODO explain
 
     Parameters
     ----------
     duration : float, optional
         Chunks duration, in seconds. Defaults to 1.
+    per_turn : int, optional
+        Number of chunks per speech turn. Defaults to 1.
+        If per_turn is greater than one, embeddings of the same speech turn
+        are averaged before classification. The intuition is that it might
+        help learn embeddings meant to be averaged/summed.
     per_label : `int`, optional
         Number of sequences per speaker in each batch. Defaults to 1.
     per_fold : `int`, optional
         Number of different speakers per batch. Defaults to 32.
+    per_epoch : `float`, optional
         Force total audio duration per epoch, in days.
         Defaults to total duration of protocol subset.
     label_min_duration : `float`, optional
@@ -119,17 +122,24 @@ class AdditiveAngularMarginLoss(Classification):
         Angular margin value. Defaults to 0.1.
     s : float, optional
         Scaling parameter value for the logits. Defaults to 7.
+
+    Reference
+    ---------
+    TODO
+
     """
 
-    def __init__(self, duration=1.0,
-                       per_label=1,
-                       per_fold=32,
+    def __init__(self, duration: float = 1.0,
+                       per_turn: int = 1,
+                       per_label: int = 1,
+                       per_fold: int = 32,
                        per_epoch: float = None,
-                       label_min_duration=0.,
-                       margin=0.1,
-                       s=7.0):
+                       label_min_duration: float = 0.,
+                       margin: float = 0.1,
+                       s: float = 7.0):
 
         super().__init__(duration=duration,
+                         per_turn=per_turn,
                          per_label=per_label,
                          per_fold=per_fold,
                          per_epoch=per_epoch,
@@ -153,33 +163,3 @@ class AdditiveAngularMarginLoss(Classification):
             self.s).to(self.device)
 
         return self.classifier_.parameters()
-
-    def batch_loss(self, batch):
-        """Compute loss for current `batch`
-
-        Parameters
-        ----------
-        batch : `dict`
-            ['X'] (`numpy.ndarray`)
-            ['y'] (`numpy.ndarray`)
-
-        Returns
-        -------
-        batch_loss : `dict`
-            ['loss'] (`torch.Tensor`) : Additive angular margin loss
-        """
-
-        # extract embeddings
-        fX = self.forward(batch)
-
-        # transform labels into tensor
-        target = torch.tensor(
-            batch['y'],
-            dtype=torch.int64,
-            device=self.device_)
-
-        # apply classification layer
-        scores = self.logsoftmax_(self.classifier_(fX, target))
-
-        # compute classification loss
-        return {'loss': self.loss_(scores, target)}

pyannote/audio/embedding/approaches/center_loss.py
@@ -31,20 +31,60 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from itertools import chain
-from pyannote.audio.embedding.generators import SpeechSegmentGenerator
-from .base import EmbeddingApproach
+from .base import RepresentationLearning
 
 
-class CenterLoss(EmbeddingApproach):
-    """Train embeddings as last hidden layer of a classifier
+class CenterDistanceModule(nn.Module):
+    """Sum of embedding-to-center cosine distances
+
+    Parameters
+    ----------
+    nfeat : int
+        Embedding size
+    nclass : int
+        Number of classes
+    """
+
+    def __init__(self, nfeat, nclass):
+        super().__init__()
+        self.centers = nn.Parameter(torch.randn(nclass, nfeat))
+        self.nfeat = nfeat
+
+    def forward(self, feat, y):
+        """Calculate the sum of cosine distances from embeddings to centers
+
+        Parameters
+        ----------
+        feat : `torch.Tensor`
+            Embedding batch
+        y : `torch.Tensor`
+            Non one-hot labels
+        Returns
+        -------
+        distance_sum : float
+            Sum of cosine distances from embeddings to centers
+        """
+        batch_size = feat.size(0)
+        feat = feat.view(batch_size, -1)
+        # Select appropriate centers for this batch's labels
+        centers_batch = self.centers.index_select(0, y.long())
+        # Return the sum of the squared distance normalized by the batch size
+        dists = 1 - F.cosine_similarity(feat, centers_batch, dim=1, eps=1e-8)
+        return torch.sum(torch.pow(dists, 2)) / 2.0 / batch_size
+
+
+class CenterLoss(RepresentationLearning):
+    """Center loss
 
     Parameters
     ----------
     duration : float, optional
         Chunks duration, in seconds. Defaults to 1.
-    loss_weight : float, optional
-        Lambda parameter controlling the effect of center distance in the loss value.
-        Defaults to 1.
+    per_turn : int, optional
+        Number of chunks per speech turn. Defaults to 1.
+        If per_turn is greater than one, embeddings of the same speech turn
+        are averaged before classification. The intuition is that it might
+        help learn embeddings meant to be averaged/summed.
     per_label : `int`, optional
         Number of sequences per speaker in each batch. Defaults to 1.
     per_fold : `int`, optional
@@ -55,29 +95,32 @@ class CenterLoss(EmbeddingApproach):
     label_min_duration : `float`, optional
         Remove speakers with less than that many seconds of speech.
         Defaults to 0 (i.e. keep them all).
+    loss_weight : float, optional
+        Lambda parameter controlling the effect of center distance in the loss value.
+        Defaults to 1.
     """
 
     CLASSIFIER_PT = '{train_dir}/weights/{epoch:04d}.classifier.pt'
     CENTERS_PT = '{train_dir}/weights/{epoch:04d}.centers.pt'
 
-    def __init__(self, duration=1.0,
-                       per_label=1,
-                       per_fold=32,
-                       loss_weight=1.,
+    def __init__(self, duration: float = 1.0,
+                       per_turn: int = 1,
+                       per_label: int = 1,
+                       per_fold: int = 32,
                        per_epoch: float = None,
-                       label_min_duration=0.):
-        super().__init__()
+                       label_min_duration: float = 0.,
+                       loss_weight: float = 1.):
+
+        super().__init__(duration=duration,
+                         per_turn=per_turn,
+                         per_label=per_label,
+                         per_fold=per_fold,
+                         per_epoch=per_epoch,
+                         label_min_duration=label_min_duration)
 
         self.loss_weight = loss_weight
-        self.per_label = per_label
-        self.per_fold = per_fold
-        self.per_epoch = per_epoch
-        self.label_min_duration = label_min_duration
-
-        self.duration = duration
-
         self.logsoftmax_ = nn.LogSoftmax(dim=1)
-        self.nll = nn.NLLLoss()
+        self.nll_ = nn.NLLLoss()
 
     def more_parameters(self):
         """Initialize trainable trainer parameters
@@ -129,31 +172,9 @@ class CenterLoss(EmbeddingApproach):
         torch.save(self.classifier_.state_dict(), classifier_pt)
         torch.save(self.center_dist_.state_dict(), centers_pt)
 
-    def get_batch_generator(self, feature_extraction,
-                            protocol, subset='train',
-                            **kwargs):
-        """Get batch generator
-
-        Parameters
-        ----------
-        feature_extraction : `pyannote.audio.features.FeatureExtraction`
-        protocol : `pyannote.database.Protocol`
-        subset : {'train', 'development', 'test'}, optional
-
-        Returns
-        -------
-        generator : `pyannote.audio.embedding.generators.SpeechSegmentGenerator`
-        """
-
-        return SpeechSegmentGenerator(
-            feature_extraction,
-            protocol,
-            subset=subset,
-            label_min_duration=self.label_min_duration,
-            per_label=self.per_label,
-            per_fold=self.per_fold,
-            per_epoch=self.per_epoch,
-            duration=self.duration)
+    @property
+    def metric(self):
+        return 'cosine'
 
     def batch_loss(self, batch):
         """Compute loss for current `batch`
@@ -167,61 +188,26 @@ class CenterLoss(EmbeddingApproach):
         Returns
         -------
         batch_loss : `dict`
-            ['loss'] (`torch.Tensor`) : Cross-entropy loss
+            ['loss'] (`torch.Tensor`) :
+            ['loss_classification'] (`torch.Tensor`) :
+            ['loss_centers'] (`torch.Tensor`) :
         """
 
-        # extract embeddings
-        fX = self.forward(batch)
+        # extract and aggregate embeddings
+        fX, y = self.embed(batch)
+
+        # distance to centers
+        loss_centers = self.center_dist_(fX, y)
 
         # apply classification layer
-        scores = self.logsoftmax_(self.classifier_(fX))
+        logits = self.logsoftmax_(self.classifier_(fX))
 
-        # get labels as a tensor
-        target = torch.tensor(
-            batch['y'],
-            dtype=torch.int64,
-            device=self.device_)
+        # compute classification loss
+        targets = torch.tensor(y, dtype=torch.int64, device=self.device)
+        loss_classification = self.nll_(logits, targets)
 
-        # compute center loss
-        return {'loss': self.nll(scores, target) + self.loss_weight * self.center_dist_(fX, target)}
-
-
-class CenterDistanceModule(nn.Module):
-    """Sum of embedding-to-center cosine distances
-
-    Parameters
-    ----------
-    nfeat : int
-        Embedding size
-    nclass : int
-        Number of classes
-    """
-
-    # TODO add support for other metrics (ex. euclidean, angular)
-
-    def __init__(self, nfeat, nclass):
-        super().__init__()
-        self.centers = nn.Parameter(torch.randn(nclass, nfeat))
-        self.nfeat = nfeat
-
-    def forward(self, feat, y):
-        """Calculate the sum of cosine distances from embeddings to centers
-
-        Parameters
-        ----------
-        feat : `torch.Tensor`
-            Embedding batch
-        y : `torch.Tensor`
-            Non one-hot labels
-        Returns
-        -------
-        distance_sum : float
-            Sum of cosine distances from embeddings to centers
-        """
-        batch_size = feat.size(0)
-        feat = feat.view(batch_size, -1)
-        # Select appropriate centers for this batch's labels
-        centers_batch = self.centers.index_select(0, y.long())
-        # Return the sum of the squared distance normalized by the batch size
-        dists = 1 - F.cosine_similarity(feat, centers_batch, dim=1, eps=1e-8)
-        return torch.sum(torch.pow(dists, 2)) / 2.0 / batch_size
+        return {
+            'loss': loss_classification + self.loss_weight * loss_centers,
+            # add this for Tensorboard comparison with other compound losses
+            'loss_classification': loss_classification,
+            'loss_center': loss_centers}

pyannote/audio/embedding/approaches/classification.py
@@ -28,18 +28,23 @@
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from .base import EmbeddingApproach
-from pyannote.audio.embedding.generators import SpeechSegmentGenerator
+from .base import RepresentationLearning
 
 
-class Classification(EmbeddingApproach):
-    """Train embeddings as last hidden layer of a classifier
+class Classification(RepresentationLearning):
+    """Classification
+
+    TODO explain
 
     Parameters
     ----------
     duration : float, optional
         Chunks duration, in seconds. Defaults to 1.
+    per_turn : int, optional
+        Number of chunks per speech turn. Defaults to 1.
+        If per_turn is greater than one, embeddings of the same speech turn
+        are averaged before classification. The intuition is that it might
+        help learn embeddings meant to be averaged/summed.
     per_label : `int`, optional
         Number of sequences per speaker in each batch. Defaults to 1.
     per_fold : `int`, optional
@@ -61,21 +66,21 @@ class Classification(EmbeddingApproach):
     CLASSIFIER_PT = '{train_dir}/weights/{epoch:04d}.classifier.pt'
 
     def __init__(self, duration: float = 1.0,
+                       per_turn: int = 1,
                        per_label: int = 1,
                        per_fold: int = 32,
                        per_epoch: float = None,
                        label_min_duration: float = 0.,
                        bias: bool = False):
-        super().__init__()
 
-        self.per_label = per_label
-        self.per_fold = per_fold
-        self.per_epoch = per_epoch
-        self.label_min_duration = label_min_duration
+        super().__init__(duration=duration,
+                         per_turn=per_turn,
+                         per_label=per_label,
+                         per_fold=per_fold,
+                         per_epoch=per_epoch,
+                         label_min_duration=label_min_duration)
+
         self.bias = bias
-
-        self.duration = duration
-
         self.logsoftmax_ = nn.LogSoftmax(dim=1)
         self.loss_ = nn.NLLLoss()
 
@@ -115,31 +120,9 @@ class Classification(EmbeddingApproach):
             train_dir=self.train_dir_, epoch=self.epoch_)
         torch.save(self.classifier_.state_dict(), classifier_pt)
 
-    def get_batch_generator(self, feature_extraction,
-                            protocol, subset='train',
-                            **kwargs):
-        """Get batch generator
-
-        Parameters
-        ----------
-        feature_extraction : `pyannote.audio.features.FeatureExtraction`
-        protocol : `pyannote.database.Protocol`
-        subset : {'train', 'development', 'test'}, optional
-
-        Returns
-        -------
-        generator : `pyannote.audio.embedding.generators.SpeechSegmentGenerator`
-        """
-
-        return SpeechSegmentGenerator(
-            feature_extraction,
-            protocol,
-            subset=subset,
-            label_min_duration=self.label_min_duration,
-            per_label=self.per_label,
-            per_fold=self.per_fold,
-            per_epoch=self.per_epoch,
-            duration=self.duration)
+    @property
+    def metric(self):
+        return 'cosine'
 
     def batch_loss(self, batch):
         """Compute loss for current `batch`
@@ -156,15 +139,17 @@ class Classification(EmbeddingApproach):
             ['loss'] (`torch.Tensor`) : Cross-entropy loss
         """
 
-        # extract embeddings
-        fX = self.forward(batch)
+        # extract and aggregate embeddings
+        fX, y = self.embed(batch)
 
         # apply classification layer
-        scores = self.logsoftmax_(self.classifier_(fX))
+        logits = self.logsoftmax_(self.classifier_(fX))
 
         # compute classification loss
-        target = torch.tensor(
-            batch['y'],
-            dtype=torch.int64,
-            device=self.device_)
-        return {'loss': self.loss_(scores, target)}
+        targets = torch.tensor(y, dtype=torch.int64, device=self.device)
+        loss_classification = self.loss_(logits, targets)
+
+        return {
+            'loss': loss_classification,
+            # add this for Tensorboard comparison with other compound losses
+            'loss_classification': loss_classification}

pyannote/audio/embedding/approaches/coco_loss.py
@@ -79,6 +79,11 @@ class CongenerousCosineLoss(Classification):
     ----------
     duration : float, optional
         Chunks duration, in seconds. Defaults to 1.
+    per_turn : int, optional
+        Number of chunks per speech turn. Defaults to 1.
+        If per_turn is greater than one, embeddings of the same speech turn
+        are averaged before classification. The intuition is that it might
+        help learn embeddings meant to be averaged/summed.
     per_label : `int`, optional
         Number of sequences per speaker in each batch. Defaults to 1.
     per_fold : `int`, optional
@@ -93,18 +98,21 @@ class CongenerousCosineLoss(Classification):
         Scaling factor used in embedding L2-normalization. Defaults to 6.25.
     """
 
-    def __init__(self, duration=1.0,
-                       per_label=1,
-                       per_fold=32,
+    def __init__(self, duration: float = 1.0,
+                       per_turn: int = 1,
+                       per_label: int = 1,
+                       per_fold: int = 32,
                        per_epoch: float = None,
-                       label_min_duration=0.,
-                       alpha=6.25):
+                       label_min_duration: float = 0.,
+                       alpha: float = 6.25):
 
         super().__init__(duration=duration,
+                         per_turn=per_turn,
                          per_label=per_label,
                          per_fold=per_fold,
                          per_epoch=per_epoch,
                          label_min_duration=label_min_duration)
+
         self.alpha = alpha
 
     def more_parameters(self):

commit 3b7b6deec12c57b82a9e9a5cdb347b6cef108851

fix: fix bugs introduced by PR #288

pyannote/audio/embedding/approaches/arcface_loss.py
@@ -59,15 +59,16 @@ class ArcLinear(nn.Module):
         self.W = nn.Parameter(torch.Tensor(nclass, nfeat))
         nn.init.xavier_uniform_(self.W)
 
-    def forward(self, x, y):
+    def forward(self, x, target=None):
         """Apply the angular margin transformation
 
         Parameters
         ----------
         x : `torch.Tensor`
             an embedding batch
-        y : `torch.Tensor`
+        target : `torch.Tensor`
             a non one-hot label batch
+
         Returns
         -------
         fX : `torch.Tensor`
@@ -76,11 +77,11 @@ class ArcLinear(nn.Module):
         # normalize the feature vectors and W
         xnorm = F.normalize(x)
         Wnorm = F.normalize(self.W)
-        y = y.long().view(-1, 1)
+        target = target.long().view(-1, 1)
         # calculate cosθj (the logits)
         cos_theta_j = torch.matmul(xnorm, torch.transpose(Wnorm, 0, 1))
         # get the cosθ corresponding to the classes
-        cos_theta_yi = cos_theta_j.gather(1, y)
+        cos_theta_yi = cos_theta_j.gather(1, target)
         # for numerical stability
         cos_theta_yi = cos_theta_yi.clamp(min=self.min_cos, max=self.max_cos)
         # get the angle separating xi and Wyi
@@ -89,7 +90,7 @@ class ArcLinear(nn.Module):
         cos_theta_yi_margin = torch.cos(theta_yi + self.margin)
         # one hot encode  y
         one_hot = torch.zeros_like(cos_theta_j)
-        one_hot.scatter_(1, y, 1.0)
+        one_hot.scatter_(1, target, 1.0)
         # project margin differences into cosθj
         return self.s * (cos_theta_j + one_hot * (cos_theta_yi_margin - cos_theta_yi))
 

pyannote/audio/embedding/approaches/center_loss.py
@@ -50,14 +50,14 @@ class CenterDistanceModule(nn.Module):
         self.centers = nn.Parameter(torch.randn(nclass, nfeat))
         self.nfeat = nfeat
 
-    def forward(self, feat, y):
+    def forward(self, feat, target=None):
         """Calculate the sum of cosine distances from embeddings to centers
 
         Parameters
         ----------
         feat : `torch.Tensor`
             Embedding batch
-        y : `torch.Tensor`
+        target : `torch.Tensor`
             Non one-hot labels
         Returns
         -------
@@ -67,7 +67,7 @@ class CenterDistanceModule(nn.Module):
         batch_size = feat.size(0)
         feat = feat.view(batch_size, -1)
         # Select appropriate centers for this batch's labels
-        centers_batch = self.centers.index_select(0, y.long())
+        centers_batch = self.centers.index_select(0, target.long())
         # Return the sum of the squared distance normalized by the batch size
         dists = 1 - F.cosine_similarity(feat, centers_batch, dim=1, eps=1e-8)
         return torch.sum(torch.pow(dists, 2)) / 2.0 / batch_size
@@ -195,16 +195,16 @@ class CenterLoss(RepresentationLearning):
 
         # extract and aggregate embeddings
         fX, y = self.embed(batch)
+        target = torch.tensor(y, dtype=torch.int64, device=self.device)
 
         # distance to centers
-        loss_centers = self.center_dist_(fX, y)
+        loss_centers = self.center_dist_(fX, target=target)
 
         # apply classification layer
-        logits = self.logsoftmax_(self.classifier_(fX))
+        logits = self.logsoftmax_(self.classifier_(fX, target=target))
 
         # compute classification loss
-        targets = torch.tensor(y, dtype=torch.int64, device=self.device)
-        loss_classification = self.nll_(logits, targets)
+        loss_classification = self.nll_(logits, target)
 
         return {
             'loss': loss_classification + self.loss_weight * loss_centers,

pyannote/audio/embedding/approaches/classification.py
@@ -32,6 +32,10 @@ import torch.nn as nn
 from .base import RepresentationLearning
 
 
+class Linear(nn.Linear):
+    def forward(self, x, target=None):
+        return super().forward(x)
+
 class Classification(RepresentationLearning):
     """Classification
 
@@ -94,7 +98,7 @@ class Classification(RepresentationLearning):
             Trainable trainer parameters
         """
 
-        self.classifier_ = nn.Linear(
+        self.classifier_ = Linear(
             self.model.dimension,
             len(self.specifications['y']['classes']),
             bias=self.bias).to(self.device)
@@ -149,13 +153,13 @@ class Classification(RepresentationLearning):
 
         # extract and aggregate embeddings
         fX, y = self.embed(batch)
+        target = torch.tensor(y, dtype=torch.int64, device=self.device)
 
         # apply classification layer
-        logits = self.logsoftmax_(self.classifier_(fX))
+        logits = self.logsoftmax_(self.classifier_(fX, target=target))
 
         # compute classification loss
-        targets = torch.tensor(y, dtype=torch.int64, device=self.device)
-        loss_classification = self.loss_(logits, targets)
+        loss_classification = self.loss_(logits, target)
 
         return {
             'loss': loss_classification,

pyannote/audio/embedding/approaches/coco_loss.py
@@ -51,13 +51,14 @@ class CocoLinear(nn.Module):
         self.alpha = alpha
         self.centers = nn.Parameter(torch.randn(nclass, nfeat))
 
-    def forward(self, x):
+    def forward(self, x, target=None):
         """Apply the angular margin transformation
 
         Parameters
         ----------
         x : `torch.Tensor`
             an embedding batch
+
         Returns
         -------
         fX : `torch.Tensor`