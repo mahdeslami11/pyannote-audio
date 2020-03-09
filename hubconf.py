da978aad62143e06e49d101a49ee89c7cffc4768
Hervé Bredin <bredin@limsi.fr>
2020-03-09 12:28:25 +0100

    feat: add "emb_voxceleb" pretrained model

--- hubconf.py
+++ hubconf.py
@@ -27,7 +27,6 @@
 # Hervé BREDIN - http://herve.niderb.fr
 
 import os
-import yaml
 import pathlib
 import typing
 import functools
@@ -44,7 +43,7 @@ _MODELS = {
     'sad_dihard': 'ee924bd1751e6960e4e4322425dcdfdc77abec33a5f3ac1b74759229c176ff70',
     'scd_dihard': '809aabd69c8116783b1d5cc1ba2f043d2b2d2fe693b34520c948ad5b2940e07c',
     'ovl_dihard': '0ae57e5fc099b498db19aabc1d4f29e21cad44751227137909bd500048830dbd',
-    'emb_voxceleb': None,
+    'emb_voxceleb': '7342eaaa39968635d81b73bd723231b677439fab0acebb7c2bd62fc687106a59',
     'sad_ami': 'cb77c5ddfeec41288f428ee3edfe70aae908240e724a44c6592c8074462c6707',
     'scd_ami': 'd2f59569c485ba3674130d441e9519993f26b7a1d3ad7d106739da0fc1dccea2',
     'ovl_ami': 'debcb45c94d36b9f24550faba35c234b87cdaf367ac25729e4d8a140ac44fe64',
