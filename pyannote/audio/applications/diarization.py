0eca336e8d9a3ce8dd5f614861502429c3b1e544
Hervé Bredin <bredin@limsi.fr>
2020-03-27 23:04:30 +0100

    wip: add first version of validation in terms of speech detection

--- pyannote/audio/applications/diarization.py
+++ pyannote/audio/applications/diarization.py
@@ -31,12 +31,21 @@ import torch
 from typing import Text
 from typing import Dict
 from pyannote.database.protocol.protocol import Protocol
+from functools import partial
+import numpy as np
+import scipy.optimize
+from pyannote.audio.features import Pretrained
+
+
+from pyannote.audio.pipeline import SpeechActivityDetection
+from .speech_detection import validate_helper_func
 
 
 class Diarization(BaseLabeling):
 
-    def validation_criterion(self, protocol: Protocol, **kwargs) -> Text:
-        return f'diarization_error_rate'
+    def validation_criterion(self, protocol: Protocol,
+                                   **kwargs) -> Text:
+        return f'detection_fscore'
 
     def validate_epoch(self,
                        epoch: int,
@@ -44,6 +53,58 @@ class Diarization(BaseLabeling):
                        device: torch.device = None,
                        batch_size: int = 32,
                        n_jobs: int = 1,
+                       duration: float = None,
+                       step: float = 0.25,
+                       detection: bool = False,
                        **kwargs) -> Dict:
-        msg = 'what should go there?'
-        raise NotImplementedError(msg)
+
+        # compute (and store) SAD scores
+        postprocess = partial(np.mean, axis=2, keepdims=True)
+        pretrained = Pretrained(validate_dir=self.validate_dir_,
+                                epoch=epoch,
+                                duration=duration,
+                                step=step,
+                                batch_size=batch_size,
+                                device=device,
+                                postprocess=postprocess)
+
+        for current_file in validation_data:
+            current_file['scores'] = pretrained(current_file)
+
+        # pipeline
+        pipeline = SpeechActivityDetection(scores="@scores",
+                                           fscore=True)
+
+        def fun(threshold):
+            pipeline.instantiate({'onset': threshold,
+                                  'offset': threshold,
+                                  'min_duration_on': 0.100,
+                                  'min_duration_off': 0.100,
+                                  'pad_onset': 0.,
+                                  'pad_offset': 0.})
+            metric = pipeline.get_metric(parallel=True)
+            validate = partial(validate_helper_func,
+                               pipeline=pipeline,
+                               metric=metric)
+            if n_jobs > 1:
+                _ = self.pool_.map(validate, validation_data)
+            else:
+                for file in validation_data:
+                    _ = validate(file)
+
+            return 1. - abs(metric)
+
+        res = scipy.optimize.minimize_scalar(
+            fun, bounds=(0., 1.), method='bounded', options={'maxiter': 10})
+
+        threshold = res.x.item()
+
+        return {'metric': self.validation_criterion(None),
+                'minimize': False,
+                'value': float(1. - res.fun),
+                'pipeline': pipeline.instantiate({'onset': threshold,
+                                                  'offset': threshold,
+                                                  'min_duration_on': 0.100,
+                                                  'min_duration_off': 0.100,
+                                                  'pad_onset': 0.,
+                                                  'pad_offset': 0.})}
