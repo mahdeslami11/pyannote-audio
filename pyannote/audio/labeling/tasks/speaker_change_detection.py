80b002a664d7c84d744bb763a62dbe484b82f136
Hervé Bredin <bredin@limsi.fr>
2020-06-09 16:34:37 +0200

    fix: take subset value into account

--- pyannote/audio/labeling/tasks/speaker_change_detection.py
+++ pyannote/audio/labeling/tasks/speaker_change_detection.py
@@ -266,7 +266,7 @@ class SpeakerChangeDetection(LabelingTask):
             protocol,
             resolution=resolution,
             alignment=alignment,
-            subset="train",
+            subset=subset,
             collar=self.collar,
             regression=self.regression,
             non_speech=self.non_speech,
