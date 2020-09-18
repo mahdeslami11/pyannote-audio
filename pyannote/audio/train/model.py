fix: do not track gradients at inference time

pyannote/audio/train/model.py
@@ -465,7 +465,8 @@ class Model(Module):
             tX = torch.tensor(batch["X"], dtype=torch.float32, device=device)
 
             # FIXME: fix support for return_intermediate
-            tfX = self(tX, return_intermediate=return_intermediate)
+            with torch.no_grad():
+                tfX = self(tX, return_intermediate=return_intermediate)
 
             tfX_npy = tfX.detach().to("cpu").numpy()
             if postprocess is not None: