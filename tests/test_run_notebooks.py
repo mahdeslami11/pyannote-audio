5512a1dba7a50faa3bbaa67eb2890c0ca22be9fb
Harry Coultas Blum <himion0@gmail.com>
2020-10-14 13:32:29 +0100

    feat: add Debug database and notebook testing
    Co-authored-by: Hervé BREDIN <hbredin@users.noreply.github.com>

---
+++ tests/test_run_notebooks.py
@@ -0,0 +1,43 @@
+from glob import glob
+
+import nbformat
+from nbconvert import PythonExporter
+
+
+def convert_notebook(notebookPath):
+    """
+    This will convert a notebook into a script
+    that we can run
+    """
+    with open(notebookPath) as fh:
+        nb = nbformat.reads(fh.read(), nbformat.NO_CONVERT)
+
+    exporter = PythonExporter()
+    source, meta = exporter.from_notebook_node(nb)
+    lines = source.split("\n")
+    nlines = []
+
+    # Remove Magic %% lines
+    # Remove !ls commands
+    for i in range(len(lines)):
+        if "get_ipython" in lines[i]:
+            continue
+        stripped = lines[i].strip()
+        if len(stripped) > 0 and stripped[0] == "!":
+            continue
+        nlines.append(lines[i])
+    source = "\n".join(nlines)
+    return source, meta
+
+
+def test_can_run_notebooks():
+    # Search for all notebooks in directory
+    notebooks = glob("**/*.ipynb")
+    for nb in notebooks:
+        src, meta = convert_notebook(nb)
+        try:
+            exec(src)
+        except Exception as e:
+            # Which notebook caused the error
+            print("Notebook: ", nb)
+            raise e
