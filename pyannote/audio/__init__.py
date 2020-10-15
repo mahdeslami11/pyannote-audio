f4a89f81e665162d9afd0c001ad8ec1f7cb4496c
Harry Coultas Blum <himion0@gmail.com>
2020-10-15 11:01:34 +0100

    feat: add versioning (pyannote.audio.__version__)

--- pyannote/audio/__init__.py
+++ pyannote/audio/__init__.py
@@ -19,3 +19,11 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
