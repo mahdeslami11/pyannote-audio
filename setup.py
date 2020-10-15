d2aee3f7c56479c2e1c860f7710db4a720e69906
Harry Coultas Blum <himion0@gmail.com>
2020-10-15 11:01:34 +0100

    feat: add versioning (pyannote.audio.__version__)

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import sys
+
+from pkg_resources import VersionConflict, require
 from setuptools import find_packages, setup
 
 with open("README.md") as f:
@@ -6,26 +9,36 @@ with open("README.md") as f:
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-setup(
-    use_scaffold=True,
-    name="pyannote.audio",
-    namespace_packages=["pyannote"],
-    packages=find_packages(),
-    install_requires=requirements,
-    description="Neural building blocks for speaker diarization",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Hervé Bredin",
-    author_email="bredin@limsi.fr",
-    url="https://github.com/pyannote/pyannote-audio",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Scientific/Engineering",
-    ],
-)
+try:
+    require("setuptools>=38.3")
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+
+    setup(
+        name="pyannote.audio",
+        namespace_packages=["pyannote"],
+        use_scm_version=True,
+        setup_requires=["setuptools_scm"],
+        packages=find_packages(),
+        install_requires=requirements,
+        description="Neural building blocks for speaker diarization",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="Hervé Bredin",
+        author_email="bredin@limsi.fr",
+        url="https://github.com/pyannote/pyannote-audio",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Topic :: Scientific/Engineering",
+        ],
+    )
