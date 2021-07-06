b20aff8d50dd17e43aaf913e9e4ad4ec4c1dd069
Hervé Bredin <bredin@limsi.fr>
2021-07-06 14:11:03 +0200

    chore: rename returns_cost to return_cost

--- pyannote/audio/utils/permutation.py
+++ pyannote/audio/utils/permutation.py
@@ -1,3 +1,26 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2021 CNRS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 from functools import singledispatch
 from typing import Callable, List, Optional, Tuple
 
@@ -8,7 +31,7 @@ from scipy.optimize import linear_sum_assignment
 
 
 @singledispatch
-def permutate(y1, y2, cost_func: Optional[Callable] = None, returns_cost: bool = False):
+def permutate(y1, y2, cost_func: Optional[Callable] = None, return_cost: bool = False):
     """Find cost-minimizing permutation
 
     Parameters
@@ -20,7 +43,7 @@ def permutate(y1, y2, cost_func: Optional[Callable] = None, returns_cost: bool =
     cost_func : callable
         Takes two (num_samples, num_classes) sequences and returns (num_classes, ) pairwise cost.
         Defaults to computing mean squared error.
-    returns_cost : bool, optional
+    return_cost : bool, optional
         Whether to return cost matrix. Defaults to False.
 
     Returns
@@ -29,22 +52,15 @@ def permutate(y1, y2, cost_func: Optional[Callable] = None, returns_cost: bool =
         (batch_size, num_samples, num_classes_1)
     permutations : list of tuple
         List of permutations so that permutation[i] == j indicates that jth speaker of y2
-        should be mapped to ith speaker of y1. permutation[i] is None if ith speaker is not
-        mapped to any speaker in y2.
+        should be mapped to ith speaker of y1. permutation[i] is None if
     cost : np.ndarray or torch.Tensor, optional
         (batch_size, num_classes_1, num_classes_2)
     """
     raise TypeError()
 
 
-# Mean Squared Error (MSE)
 def mse_cost_func(Y, y):
     return torch.mean(F.mse_loss(Y, y, reduction="none"), axis=0)
-
-
-# Maximum Absolute Difference (MAD)
-def mad_cost_func(Y, y):
-    return torch.max(torch.abs(Y - y), axis=0)[0]
 
 
 @permutate.register
@@ -52,7 +68,7 @@ def permutate_torch(
     y1: torch.Tensor,
     y2: torch.Tensor,
     cost_func: Optional[Callable] = None,
-    returns_cost: bool = False,
+    return_cost: bool = False,
 ) -> Tuple[torch.Tensor, List[Tuple[int]]]:
 
     batch_size, num_samples, num_classes_1 = y1.shape
@@ -75,7 +91,7 @@ def permutate_torch(
     permutations = []
     permutated_y2 = []
 
-    if returns_cost:
+    if return_cost:
         costs = []
 
     permutated_y2 = torch.zeros(y1.shape, device=y2.device, dtype=y2.dtype)
@@ -106,10 +122,10 @@ def permutate_torch(
                 permutated_y2[b, :, k1] = y2_[:, k2]
         permutations.append(tuple(permutation))
 
-        if returns_cost:
+        if return_cost:
             costs.append(cost)
 
-    if returns_cost:
+    if return_cost:
         return permutated_y2, permutations, torch.stack(costs)
 
     return permutated_y2, permutations
@@ -120,17 +136,17 @@ def permutate_numpy(
     y1: np.ndarray,
     y2: np.ndarray,
     cost_func: Optional[Callable] = None,
-    returns_cost: bool = False,
+    return_cost: bool = False,
 ) -> Tuple[np.ndarray, List[Tuple[int]]]:
 
     output = permutate(
         torch.from_numpy(y1),
         torch.from_numpy(y2),
         cost_func=cost_func,
-        returns_cost=returns_cost,
+        return_cost=return_cost,
     )
 
-    if returns_cost:
+    if return_cost:
         permutated_y2, permutations, costs = output
         return permutated_y2.numpy(), permutations, costs.numpy()
 
